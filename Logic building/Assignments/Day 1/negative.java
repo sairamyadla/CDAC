d8899a9864eafc4744d99976b38c07fa1699dbd2
Sai Ram Yadla <94898052+sairamyadla@users.noreply.github.com>
2024-08-22 15:07:45 +0530

    Add files via upload

---
+++ Logic building/Assignments/Day 1/negative.java
@@ -0,0 +1,13 @@
+class negative{
+	public static void main(String args[]){
+		
+		int number = -15; 
+		if(number < 0){
+		System.out.println( "The number is Negative");
+		}
+		else{
+		System.out.println( "The number is Positive");		
+		}
+	}
+
+}
---
+++ Logic building/Assignments/Day 1/positive.java
@@ -0,0 +1,13 @@
+class positive{
+	public static void main(String args[]){
+		
+		int number = 10; 
+		if(number > 0){
+		System.out.println( "The number is Positive");
+		}
+		else{
+		System.out.println( "The number is Negative");		
+		}
+	}
+
+}
